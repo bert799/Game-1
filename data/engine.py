697071d8921e88687beeefe69380615ba042b5dc
Bernardo Cunha Capoferri <bernardocc@al.insper.edu.br>
2020-06-12 22:31:12 -0300

    jogo organizado em dois arquivos

---
+++ data/engine.py
@@ -0,0 +1,324 @@
+import pygame, math, os
+from pygame.locals import *
+
+global e_colorkey
+e_colorkey = (255,255,255)
+
+def set_global_colorkey(colorkey):
+    global e_colorkey
+    e_colorkey = colorkey
+
+# inicio do código sobre a física do jogo
+
+# colisões retorna o nome do bloco colidido para identificação
+def collision_test(object_1, object_list, names):
+    collision_list = []
+    name = ''
+    for obj in object_list:
+        if obj.colliderect(object_1):
+            collision_list.append(obj)
+            for key in names:
+                if obj in names[key]:
+                    name = key
+    return collision_list, name
+
+# função modifica o mapa para que os baus que foram abertos fiquem abertos
+def abre_o_bau(map_file, num_chest):
+    line_chest = (tile_names['C'][num_chest].y)/16
+    with open(map_file, 'r') as map_data:
+        blocos = map_data.readlines()
+        location_chest = str(blocos[int(line_chest)])
+        change_chest = location_chest.replace('C', 'O') 
+        blocos[int(line_chest)] = change_chest
+    with open(map_file, 'w') as map_data:
+        map_data.writelines(blocos)
+
+# Física
+class physics_obj(object):
+   
+    def __init__(self,x,y,x_size,y_size):
+        self.width = x_size
+        self.height = y_size
+        self.rect = pygame.Rect(x,y,self.width,self.height)
+        self.x = x
+        self.y = y
+    # funçao que recebe colisão e define reação   
+    def move(self,movement,platforms,names):
+        self.x += movement[0]
+        self.rect.x = int(self.x)
+        block_hit_list, block_type = collision_test(self.rect,platforms,names)
+        collision_types = {'top':False,'bottom':False,'right':False,'left':False,'slant_bottom':False,'data':[]}
+        for block in block_hit_list:
+            markers = [False,False,False,False]
+            if movement[0] > 0 and block_type != '2' and block_type !='D' and block_type !='C' and block_type !='O':
+                self.rect.right = block.left
+                collision_types['right'] = True
+                markers[0] = True
+            elif movement[0] < 0 and block_type != '2' and block_type !='D' and block_type !='C' and block_type !='O':
+                self.rect.left = block.right
+                collision_types['left'] = True
+                markers[1] = True
+            collision_types['data'].append([block,markers])
+            self.x = self.rect.x
+        self.y += movement[1]
+        self.rect.y = int(self.y)
+        block_hit_list, block_type = collision_test(self.rect,platforms,names)
+        for block in block_hit_list:
+            markers = [False,False,False,False]
+            if movement[1] > 0 and block_type !='D' and block_type !='C' and block_type !='O':
+                self.rect.bottom = block.top
+                collision_types['bottom'] = True
+                markers[2] = True
+            elif movement[1] < 0 and block_type != '2' and block_type !='D' and block_type !='C' and block_type !='O':
+                self.rect.top = block.bottom
+                collision_types['top'] = True
+                markers[3] = True
+            collision_types['data'].append([block,markers])
+            self.change_y = 0
+            self.y = self.rect.y
+        return collision_types
+
+#detecta colisões
+
+class cuboid(object):
+    
+    def __init__(self,x,y,z,x_size,y_size,z_size):
+        self.x = x
+        self.y = y
+        self.z = z
+        self.x_size = x_size
+        self.y_size = y_size
+        self.z_size = z_size
+        
+    def set_pos(self,x,y,z):
+        self.x = x
+        self.y = y
+        self.z = z
+        
+    def collidecuboid(self,cuboid_2):
+        cuboid_1_xy = pygame.Rect(self.x,self.y,self.x_size,self.y_size)
+        cuboid_1_yz = pygame.Rect(self.y,self.z,self.y_size,self.z_size)
+        cuboid_2_xy = pygame.Rect(cuboid_2.x,cuboid_2.y,cuboid_2.x_size,cuboid_2.y_size)
+        cuboid_2_yz = pygame.Rect(cuboid_2.y,cuboid_2.z,cuboid_2.y_size,cuboid_2.z_size)
+        if (cuboid_1_xy.colliderect(cuboid_2_xy)) and (cuboid_1_yz.colliderect(cuboid_2_yz)):
+            return True
+        else:
+            return False
+
+#função Geral
+
+def simple_Geral(x,y,e_type):
+    return Geral(x,y,1,1,e_type)
+
+def flip(img,boolean=True):
+    return pygame.transform.flip(img,boolean,False)
+ 
+def blit_center(surf,surf2,pos):
+    x = int(surf2.get_width()/2)
+    y = int(surf2.get_height()/2)
+    surf.blit(surf2,(pos[0]-x,pos[1]-y))
+ 
+class Geral(object):
+    global animation_database, animation_higher_database
+   
+    def __init__(self,x,y,size_x,size_y,e_type): 
+        self.x = x
+        self.y = y
+        self.size_x = size_x
+        self.size_y = size_y
+        self.obj = physics_obj(x,y,size_x,size_y)
+        self.animation = None
+        self.image = None
+        self.animation_frame = 0
+        self.animation_tags = []
+        self.flip = False
+        self.offset = [0,0]
+        self.rotation = 0
+        self.type = e_type 
+        self.action_timer = 0
+        self.action = ''
+        self.set_action('idle') 
+        self.Geral_data = {}
+        self.alpha = None
+ 
+    def set_pos(self,x,y):
+        self.x = x
+        self.y = y
+        self.obj.x = x
+        self.obj.y = y
+        self.obj.rect.x = x
+        self.obj.rect.y = y
+ 
+    def move(self,momentum,platforms, names):
+        collisions = self.obj.move(momentum,platforms, names)
+        self.x = self.obj.x
+        self.y = self.obj.y
+        return collisions
+ 
+    def rect(self):
+        return pygame.Rect(self.x,self.y,self.size_x,self.size_y)
+ 
+    def set_flip(self,boolean):
+        self.flip = boolean
+ 
+    def set_animation_tags(self,tags):
+        self.animation_tags = tags
+ 
+    def set_animation(self,sequence):
+        self.animation = sequence
+        self.animation_frame = 0
+ 
+    def set_action(self,action_id,force=False):
+        if (self.action == action_id) and (force == False):
+            pass
+        else:
+            self.action = action_id
+            anim = animation_higher_database[self.type][action_id]
+            self.animation = anim[0]
+            self.set_animation_tags(anim[1])
+            self.animation_frame = 0
+
+    def get_Geral_angle(Geral_2):
+        x1 = self.x+int(self.size_x/2)
+        y1 = self.y+int(self.size_y/2)
+        x2 = Geral_2.x+int(Geral_2.size_x/2)
+        y2 = Geral_2.y+int(Geral_2.size_y/2)
+        angle = math.atan((y2-y1)/(x2-x1))
+        if x2 < x1:
+            angle += math.pi
+        return angle
+
+    def get_center(self):
+        x = self.x+int(self.size_x/2)
+        y = self.y+int(self.size_y/2)
+        return [x,y]
+ 
+    def clear_animation(self):
+        self.animation = None
+ 
+    def set_image(self,image):
+        self.image = image
+ 
+    def set_offset(self,offset):
+        self.offset = offset
+ 
+    def set_frame(self,amount):
+        self.animation_frame = amount
+ 
+    def handle(self):
+        self.action_timer += 1
+        self.change_frame(1)
+ 
+    def change_frame(self,amount):
+        self.animation_frame += amount
+        if self.animation != None:
+            while self.animation_frame < 0:
+                if 'loop' in self.animation_tags:
+                    self.animation_frame += len(self.animation)
+                else:
+                    self.animation = 0
+            while self.animation_frame >= len(self.animation):
+                if 'loop' in self.animation_tags:
+                    self.animation_frame -= len(self.animation)
+                else:
+                    self.animation_frame = len(self.animation)-1
+ 
+    def get_current_img(self):
+        if self.animation == None:
+            if self.image != None:
+                return flip(self.image,self.flip)
+            else:
+                return None
+        else:
+            return flip(animation_database[self.animation[self.animation_frame]],self.flip)
+
+    def get_drawn_img(self):
+        image_to_render = None
+        if self.animation == None:
+            if self.image != None:
+                image_to_render = flip(self.image,self.flip).copy()
+        else:
+            image_to_render = flip(animation_database[self.animation[self.animation_frame]],self.flip).copy()
+        if image_to_render != None:
+            center_x = image_to_render.get_width()/2
+            center_y = image_to_render.get_height()/2
+            image_to_render = pygame.transform.rotate(image_to_render,self.rotation)
+            if self.alpha != None:
+                image_to_render.set_alpha(self.alpha)
+            return image_to_render, center_x, center_y
+ 
+    def display(self,surface,scroll):
+        image_to_render = None
+        if self.animation == None:
+            if self.image != None:
+                image_to_render = flip(self.image,self.flip).copy()
+        else:
+            image_to_render = flip(animation_database[self.animation[self.animation_frame]],self.flip).copy()
+        if image_to_render != None:
+            center_x = image_to_render.get_width()/2
+            center_y = image_to_render.get_height()/2
+            image_to_render = pygame.transform.rotate(image_to_render,self.rotation)
+            if self.alpha != None:
+                image_to_render.set_alpha(self.alpha)
+            blit_center(surface,image_to_render,(int(self.x)-scroll[0]+self.offset[0]+center_x,int(self.y)-scroll[1]+self.offset[1]+center_y))
+ 
+# animações
+
+global animation_database
+animation_database = {}
+ 
+global animation_higher_database
+animation_higher_database = {}
+#[1,2], onde 1 é o nome da imagem e 2 o tempo
+def animation_sequence(sequence,base_path,colorkey=(255,255,255),transparency=255):
+    global animation_database
+    result = []
+    for frame in sequence:
+        image_id = base_path + base_path.split('/')[-2] + '_' + str(frame[0])
+        image = pygame.image.load(image_id + '.png').convert()
+        image.set_colorkey(colorkey)
+        image.set_alpha(transparency)
+        animation_database[image_id] = image.copy()
+        for i in range(frame[1]):
+            result.append(image_id)
+    return result
+ 
+ 
+def get_frame(ID):
+    global animation_database
+    return animation_database[ID]
+ 
+def load_animations(path):
+    global animation_higher_database, e_colorkey
+    f = open(path + 'geral_animations.txt','r')
+    data = f.read()
+    f.close()
+    for animation in data.split('\n'):
+        sections = animation.split(' ')
+        anim_path = sections[0]
+        Geral_info = anim_path.split('/')
+        Geral_type = Geral_info[0]
+        animation_id = Geral_info[1]
+        timings = sections[1].split(';')
+        tags = sections[2].split(';')
+        sequence = []
+        n = 0
+        for timing in timings:
+            sequence.append([n,int(timing)])
+            n += 1
+        anim = animation_sequence(sequence,path + anim_path,e_colorkey)
+        if Geral_type not in animation_higher_database:
+            animation_higher_database[Geral_type] = {}
+        animation_higher_database[Geral_type][animation_id] = [anim.copy(),tags]        
+
+# função q troca a cor
+
+def swap_color(img,old_c,new_c):
+    global e_colorkey
+    img.set_colorkey(old_c)
+    surf = img.copy()
+    surf.fill(new_c)
+    surf.blit(img,(0,0))
+    surf.set_colorkey(e_colorkey)
+    return surf
+
